e5666f526349ec2be7c9ec3f19f5b608ec26d6b2
Simon Micheneau <contact@simon-micheneau.fr>
2018-06-06 16:17:16 +0200

    début de config webpack pour le css et sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,21 @@
 const path = require("path")
 const UglifyJSPlugin = require("uglifyjs-webpack-plugin")
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const dev = process.env.NODE_ENV === "dev"
 
+let cssLoaders = [
+  {loader: 'css-loader', options: {minimize: !dev}}
+]
+
 let config = {
-    entry: "./front/js/app.js",
+    entry: "./front/assets/app.js",
     mode: 'development',
     watch: dev,
     output: {
         path: path.resolve("./public/js"),
         filename: "bundle.js"
     },
+    devtool: dev ? "cheap-module-eval-source-map" : false,
     module: {
         rules: [
             {
@@ -21,10 +27,26 @@ let config = {
                         presets: ['@babel/preset-env']
                     }
                 }
+            }, {
+                test:/\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: cssLoaders
+                })
+            }, {
+                test:/\.(scss|sass)$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: [...cssLoaders, 'sass-loader']
+                })
             }
         ]
     },
-    plugins: []
+    plugins: [
+        new ExtractTextPlugin({
+            filename: "[name].css"
+        }),
+    ]
 };
 
 if (!dev) {
